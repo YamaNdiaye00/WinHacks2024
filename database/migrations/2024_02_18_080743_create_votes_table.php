8661abd135e490c949cfbd98561f9c1c3ef32ade
Yama <pyama.ndiaye@gmail.com>
2024-02-18 03:08:23 -0500

    vote recreations

--- database/migrations/2024_02_18_080743_create_votes_table.php
+++ database/migrations/2024_02_18_080743_create_votes_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->unsignedBigInteger('score'); // The score voted by a user
             $table->timestamps();
         });
-
     }
 
     /**
